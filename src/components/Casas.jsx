0193048c94ae2f856314523742861e4e963fa69b
i-am-iron-man <fabianvanderstoel12@gmail.com>
2024-01-31 20:28:12 +0100

    esto es mi primer commit

--- src/components/Casas.jsx
+++ src/components/Casas.jsx
@@ -1,9 +1,26 @@
+import React, {useEffect, useState} from 'react'
+import { todosPersonajes } from '../funciones/funcion'
+
+
+
 
 const Casas = () => {
+  const [casas, setCasas] = useState([])
+  console.log(casas);
+
+useEffect(()=> {
+    todosPersonajes(setCasas)
+}, [])
+
   return (
-    <div>
-      Casas
-    </div>
+    <>
+    {casas.map((casa, index) => (
+      <div key={index}>
+        <a href={`/casas/${casa.id}`}>{casa.name}</a>
+        <img src={casa.image} alt="imagen" />
+      </div>
+    ))}
+    </>
   )
 }
 
