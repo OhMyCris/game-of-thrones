703eb5fa3dd9ee51f47efadb9ee0b7ee3baf0962
OhMyCris <cristina.crespo@bootcamp-upgrade.com>
2024-01-31 23:52:43 +0100

    cronologia funcional y bonita

--- src/components/Cronologia.jsx
+++ src/components/Cronologia.jsx
@@ -1,8 +1,53 @@
+import { useContext, useEffect, useRef, useState } from 'react';
+import './Cronologia.css'
+import 'simplebar/dist/simplebar.min.css';
+import SimpleBar from 'simplebar-react'
+import { GameOfContext } from './context/context';
 
 const Cronologia = () => {
+  const { characters } = useContext(GameOfContext);
+  const [character, setCharacter] = useState([]);
+  const [ascendingOrder, setAscendingOrder] = useState(true);
+  // const {t} = useTranslation();
+
+  useEffect(() => {
+    const sorted = characters.sort((a, b) => {
+      return ascendingOrder ? a.age - b.age : b.age - a.age});
+    
+      const newSorted = sorted.map((obj, index) => ({...obj, id: index + 1}))
+
+    setCharacter(newSorted);
+    console.log(newSorted);
+  }, [ascendingOrder, characters])
+
+  const cambio = () => {
+    setAscendingOrder((prevOrder) => !prevOrder)
+  }
+
+  const simpleBarRef = useRef(null);
+  useEffect(() => {
+    // Actualiza la barra de desplazamiento después de que el componente se monta
+    if (simpleBarRef.current) {
+      simpleBarRef.current.recalculate();
+    }
+  }, []);
+
   return (
     <div>
-      Cronologia
+      <div onClick={cambio} className='cursor'>
+        {ascendingOrder ? '\u2191' : '\u2193'}
+      </div>
+      <SimpleBar style={{ maxHeight: '720px', maxWidth:'width: 800px'}} ref={simpleBarRef}>
+        <div className='orden'>
+          {character.map((chara) =>(
+            <div key={chara.id} className={chara.id % 2 === 0 ? 'clear' : 'caja'}>
+              <p>{chara.age ? chara.age : '??'}</p>
+              <p>{chara.name}</p>
+              <img src={chara.image} alt={chara.name} className='circulos'/>
+            </div>
+          ))}
+        </div>
+      </SimpleBar>
     </div>
   )
 }
