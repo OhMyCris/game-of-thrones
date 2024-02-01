aa591ac92731c287f3632c54ab6ee80b1bbc4e6d
i-am-iron-man <fabianvanderstoel12@gmail.com>
2024-02-01 21:35:36 +0100

    quinto comentario

--- src/components/DetailCasas.jsx
+++ src/components/DetailCasas.jsx
@@ -1,16 +1,31 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-
-
+import { unicaCasa } from "../funciones/funcion"
+import "./DetailCharacters.css"
 const DetailCasas = () => {
+  const [house, setHouse] = useState([])
 
 const params = useParams()
 
+useEffect(()=>{
+  unicaCasa(params.id, setHouse)
+}, [])
 
   return (
     <div>
-       <p>ID: {params.id}</p>
-      <p>Settlement: {params.settlement}</p>
+      <button><a href="/casas">salir</a></button>
+      <img src={house.image} alt="imagen" />
+      <p>{house.name}</p>
+
+      <div className="contenedor-detalles">
+       <p className="parrafo1">Region <br />{house.region}</p>
+       <p>Settlement <br />{house.settlement}</p>
+       <p>Alliances <br />{house.alliances}</p>
+       <p>Religions <br />{house.religions}</p>
+       <p>Foundation <br />{house.foundation}</p>
+       
+      </div>
+       
     </div>
   )
 }
--- src/funciones/funcion.jsx
+++ src/funciones/funcion.jsx
@@ -5,4 +5,8 @@ const todosPersonajes = async(state) => {
     state(peticion.data);
 }
 
-export{todosPersonajes}
+const unicaCasa = async (id, state) => {
+    const peticion = await axios.get(`https://game-of-thrones-json-server.vercel.app/houses/${id}`  )
+    state(peticion.data);
+}
+export{todosPersonajes, unicaCasa}
