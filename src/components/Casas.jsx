a599b15ba68b10d97735210d9f0ed578cd16f2e3
i-am-iron-man <fabianvanderstoel12@gmail.com>
2024-02-01 16:26:22 +0100

    tercer commit

--- src/components/Casas.jsx
+++ src/components/Casas.jsx
@@ -7,21 +7,42 @@ import "./casas.css"
 
 const Casas = () => {
   const [casas, setCasas] = useState([])
-  console.log(casas);
+  const [search, setSearch] = useState("")
+  
+  //console.log(casas);
+
+  const searcher = (e) => {
+    setSearch(e.target.value)
+    console.log(e.target.value);
+  }
+
+  let result = []
+  if(!search){
+    result = casas
+  }else{
+     result = casas.filter((dato) =>
+    dato.name.toLowerCase().includes(search.toLowerCase())
+    )
+  }
 
 useEffect(()=> {
     todosPersonajes(setCasas)
 }, [])
 
   return (
-    <section>
-    {casas.map((casa, index) => (
-      <div key={index} className='contenedor'>
-        <a href={`/casas/${casa.id}`}>{casa.name}</a>
-        <img src={casa.image} alt="imagen" className='escudos' />
-      </div>
-    ))}
-    </section>
+    <>
+    <input value={search} onChange={searcher} type='text' placeholder='search'></input>
+
+<section>
+{result.map((casa, index) => (
+  <div key={index} className='contenedor'>
+    <a href={`/casas/${casa.id}`}>{casa.name}</a>
+    <img src={casa.image} alt="imagen" className='escudos' />
+  </div>
+))}
+</section>
+    </>
+    
   )
 }
 
