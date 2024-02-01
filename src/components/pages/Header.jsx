9de187f19b51f7f2505bb3e37e60e36b492cdfd2
OhMyCris <cristina.crespo@bootcamp-upgrade.com>
2024-02-01 21:41:28 +0100

    db y botones que aparecen y desparecen a medias

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import "./Header.css"
+import { useEffect, useState } from "react";
 
 
 const Header = () => {
@@ -11,11 +12,40 @@ const Header = () => {
     i18n.changeLanguage(lang)
   }
 
+  const [mostrarHome, setMostrarHome] = useState(false)
+  const [mostrarVolver, setMostrarVolver] = useState(false)
+  const location = useLocation()
+
+  const rutasFijas = ['/characters', '/casas', '/cronologia']
+  const rutasDinamicas = ['/characters/:id', '/casas/:id']
+
+  useEffect(() => {
+    const rutaFija = rutasFijas.includes(location.pathname);
+    const rutaDinamica = rutasDinamicas.includes(location.pathname);
+
+    setMostrarHome(rutaFija || rutaDinamica)
+  }, [location.pathname])
+
+  useEffect(() => {
+    const rutaDinamica = rutasDinamicas.includes(location.pathname);
+
+    setMostrarVolver(rutaDinamica)
+  }, [location.pathname])
+
   return (
     <header>
-      <Link to='/'>
+      {/* {mostrarVolver && ( */}
+        <Link to='/characters'>
+          <div className="volver">
+            <p>←  Volver</p>
+          </div>
+        </Link>
+      {/* )} */}
+      {mostrarHome && (
+        <Link to='/'>
           <img src="./public/images/home.png"/>
         </Link>
+      )}
         <img className="espacio" src="./public/images/spain.png" onClick={() => handleLanguageChange('es')}/>
         <img className="espacio" src="./public/images/united-kingdom.png" onClick={() => handleLanguageChange('en')}/>
     </header>
